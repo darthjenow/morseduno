01201165fed1b685f05be5483de44cec628a418e
DarthJenow <darthjenow@gmail.com>
2022-03-03 22:26:24 +0100

    implemented off switch

--- README.md
+++ README.md
@@ -3,11 +3,8 @@ morseduno is a project centered around an Arduino Nano / Uno sending out morse m
 
 It can be programmed over serial, for example with the included python file.
 
-## TODO
-- add hardware repeat switch
-
 ## Encoding to save space
-In Order to save space in the RAM and EEPROM the data is stored in a space efficient way.
+In order to save space the data is stored in a space efficient way.
 
 ### Morsecodes.h
 The header-file `Morsecodes.h` contains the morse-codes for the supported chars and symbols. The entries are mapped to the ASCII-range from 33 (_exclamation mark_) to 95 (_underscore_).
--- morseduno.ino
+++ morseduno.ino
@@ -1,5 +1,6 @@
 #include <EEPROM.h>
 #include <math.h>
+#include <Bounce2.h>
 
 #include "Morsecodes.h"
 
@@ -14,6 +15,7 @@
 
 #define MORSE_PIN_LED LED_BUILTIN // pin for visual morsing
 #define MORSE_PIN_BEEP 11 // pin for PWM (audio) morsing
+#define MORSE_ENABLE 12 // enables or disables the morsing
 
 #define BAUD_RATE 9600 // baud-rate for the serial communication
 
@@ -26,6 +28,8 @@ byte morse_millis_i = 0;
 
 bool serial_string_in_progress = false;
 
+Bounce morse_enable_bounce = Bounce();
+
 // timer interrupt
 ISR(TIMER1_COMPA_vect) {
 	TCNT1 = 0; // Reset the timer
@@ -37,6 +41,7 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 byte get_next_morse_millis() {
+
 	do {
 		morse_millis_i++;
 
@@ -91,7 +96,12 @@ void load_next_morse_char() {
 	}
 }
 
-// retrieves the mrose_code from the header-file identified by the char
+/**
+ * @brief Retrieve the morse-code for a char from the header-file
+ * 
+ * @param c char of which the code should be retrieved
+ * @return int morse representation of the char
+ */
 int get_morse_char(char c) {
 	// if the letter is lower-case, make it uppercase
 	if (c >= 97) {
@@ -101,14 +111,22 @@ int get_morse_char(char c) {
 	return morse_codes[c - 33];
 }
 
-// save a char to the message array
+/**
+ * @brief save a char to the message array
+ * 
+ * @param c char to save
+ */
 void add_char_to_message(char c) {
 	message[message_length] = c;
 
 	message_length++;
 }
 
-// controls the GPIO-output for morsing signals
+/**
+ * @brief controls the GPIO-output for morsing signals
+ * 
+ * @param state wether the morse-code is ON or OFF
+ */
 void do_morse(bool state) {
 	// toggle the LED
 	digitalWrite(MORSE_PIN_LED, state);
@@ -117,19 +135,48 @@ void do_morse(bool state) {
 	analogWrite(MORSE_PIN_BEEP, state * 127);
 }
 
-// save the length of the message to the EEPROM
+/**
+ * @brief save the current message to EEPROM
+ * 
+ */
 void save_to_eeprom() {
 	EEPROM.put(0, message_length);
 	EEPROM.put(2, message);
 }
 
-// load the last morse-string from EEPROM
+/**
+ * @brief load the morse-string from EEPROM
+ * 
+ */
 void load_from_EEPROM() {
 	EEPROM.get(0, message_length);
 	EEPROM.get(2, message);
 }
 
-// the setup function runs once when you press reset or power the board
+void morse_enable(bool state) {
+	if (state) {
+		// restart the timer
+		TCCR1B |= B00000101;
+
+	} else {
+		// stop the timer
+		TCCR1B &= B11111000;
+
+		//immidiately turn all the output off
+		do_morse(false);
+
+		// reset the counters
+		message_i = 0;
+		morse_millis_i = -1;
+
+		load_next_morse_char(); // load the first char into the buffer
+	}
+}
+
+/**
+ * @brief Initialize the pins, serial, timer and message from EEPROM
+ * 
+ */
 void setup() {
 	// Start the serial communication with the specified baudrate
 	Serial.begin(BAUD_RATE);
@@ -138,7 +185,11 @@ void setup() {
 	pinMode(MORSE_PIN_LED, OUTPUT);
 	pinMode(MORSE_PIN_BEEP, OUTPUT);
 
-	delay(2000);
+	// initialize the toggle-switch with debouncing
+	morse_enable_bounce.attach(MORSE_ENABLE, INPUT_PULLUP);
+	morse_enable_bounce.interval(5);
+
+	delay(1000);
 
 	// retrieve the data from the EEPROM
 	load_from_EEPROM();
@@ -148,7 +199,8 @@ void setup() {
 	cli();
 
 	TCCR1A = 0; // Reset entire TC1A register
-	TCCR1B = B00000101; // Reset entire TCCR1B register and set Prescalar = 1024
+	// morse_enable(true); // enable the timer
+	TCCR1B = B00000101;
 
 	TIMSK1 |= B00000010; // Set OCIE1A to 1 so we enable compare match A
 
@@ -159,11 +211,20 @@ void setup() {
 	sei();
 }
 
-// the loop function runs over and over again forever
 void loop() {
+	morse_enable_bounce.update();
+
+	if (morse_enable_bounce.changed()) {
+		morse_enable(morse_enable_bounce.read());
+	}
+
 	delay(50);
 }
 
+/**
+ * @brief Respons to incoming serial messages by saving by concating them into one message
+ * 
+ */
 void serialEvent() {
 	while (Serial.available()) {
 		char c = Serial.read();
